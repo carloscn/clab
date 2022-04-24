04c05e8fc1bad6bbfddedbf75a3de6e1fea04f5a
Carlos Wei <carlos.wei.hk@gmail.com>
2022-04-24 15:51:29 +0800

    [clab] libc test
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

---
+++ macos/test_clib/temp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void (*ctor_t)(void);
+
+void __attribute__((constructor)) my_init(void)
+{
+    printf("my init!\n");
+}
+void __attribute__((destructor)) my_exit(void) {
+    printf("my exit!\n");
+}
+
+void atexit_func(void) {
+    printf("atexit !\n");
+}
+
+int main(void)
+{
+    atexit(&atexit_func);
+    printf("main will return\n");
+    return 0;
+}
