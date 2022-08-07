a460ff37b51819393b9c5b2c8c3dab3b4111ab60
Carlos Wei <carlos.wei.hk@gmail.com>
2022-08-07 02:39:01 -0700

    [linux] add test_pid
    test fork a process.
    create a zombie process.
    avoid a zombie child process.
    
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

--- linux/test_pid/test_pid_wait.c
+++ linux/test_pid/test_pid_wait.c
@@ -8,13 +8,23 @@
 int main(int argc, char *argv[])
 {
     pid_t pid = fork();
+    u_int16_t count = 0;
 
     while(1) {
         if (0 == pid) {
             debug_log("Current PID = %d, PPID = %d\n", getpid(), getppid());
-            debug_log("I'm Child process!\n");
+            debug_log("I'm Child process\n");
             sleep(1);
+            count ++;
+            if (count > 10) {
+                debug_log("child will exit.\n");
+                exit(0);
+            }
         } else if (pid > 0) {
+            debug_log("-------------------> wait for child exiting.\n");
+            // WNOHANG is non block mode.
+            // WUNTRACED is block mode.
+            pid_t t_pid = waitpid(pid, NULL, WUNTRACED);
             debug_log("-------------------> Current PID = %d, PPID = %d\n", getpid(), getppid());
             debug_log("-------------------> I'm Parent process!\n");
             sleep(2);
--- linux/test_pid/test_pid_zombie.c
+++ linux/test_pid/test_pid_zombie.c
@@ -8,13 +8,26 @@
 int main(int argc, char *argv[])
 {
     pid_t pid = fork();
+    u_int16_t count = 0;
 
     while(1) {
         if (0 == pid) {
             debug_log("Current PID = %d, PPID = %d\n", getpid(), getppid());
-            debug_log("I'm Child process!\n");
+            debug_log("I'm Child process\n");
+            // just exit child process, and parent process don't call waitpid(),
+            // which to create zombie process
             sleep(1);
+            count ++;
+            if (count > 10) {
+                debug_log("child will exit.\n");
+                exit(0);
+            }
         } else if (pid > 0) {
+            // debug_log("-------------------> wait for child exiting.\n");
+            // WNOHANG is non block mode.
+            // WUNTRACED is block mode.
+            // mask the next line code, the zombie process will be generated.
+            // pid_t t_pid = waitpid(pid, NULL, WUNTRACED);
             debug_log("-------------------> Current PID = %d, PPID = %d\n", getpid(), getppid());
             debug_log("-------------------> I'm Parent process!\n");
             sleep(2);
