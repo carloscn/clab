5186868502cb8efbcbeaf037b1764c0195e9c533
Carlos Wei <carlos.wei.hk@gmail.com>
2022-09-04 04:14:17 -0700

    [linux] malloc mmap
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

--- linux/test_memory/malloc_failed.c
+++ linux/test_memory/malloc_failed.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <malloc.h>
 
-int main() {
+
+static int malloc_use_brk(void)
+{
   printf("使用cat /proc/%d/maps查看内存分配\n", getpid());
 
   //申请1字节的内存
@@ -18,4 +20,27 @@ int main() {
 
   getchar();
   return 0;
+}
+
+static int malloc_use_mmap() {
+  //申请1字节的内存
+  void *addr = malloc(1280*1024);
+  printf("此128KB字节的内存起始地址：%p\n", addr);
+  printf("使用cat /proc/%d/maps查看内存分配\n",getpid());
+
+  //将程序阻塞，当输入任意字符时才往下执行
+  getchar();
+
+  //释放内存
+  free(addr);
+  printf("释放了128KB字节的内存，内存也归还给了操作系统\n");
+
+  getchar();
+  return 0;
+}
+
+int main() {
+
+    //return malloc_use_brk();
+    return malloc_use_mmap();
 }
