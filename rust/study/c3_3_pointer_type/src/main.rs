2fc546b6ce9fa037180de7975bb95bef488bc944
Carlos <carlos.wei.hk@gmail.com>
2023-02-02 11:14:19 +0800

    [rust] string type
    Signed-off-by: Carlos <carlos.wei.hk@gmail.com>

--- rust/study/c3_3_pointer_type/src/main.rs
+++ rust/study/c3_3_pointer_type/src/main.rs
@@ -154,10 +154,126 @@ fn vector_type() {
     assert_eq!(k.pop(), Some(3));
 }
 
+fn my_print(n: &[f64]) {
+    for elt in n {
+        println!("{}", elt);
+    }
+    print!("\n\n");
+}
+
+fn slice_type() {
+    // define a vector on heap
+    let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
+    // define an array on stack
+    let a: [f64; 4] = [0.0, -0.707, -1.0, -0.707];
+
+    let slice_v: &[f64] = &v;
+    let slice_a: &[f64] = &a;
+
+    my_print(slice_v); // works on vectors
+    my_print(slice_a); // works on arrays
+
+    // we can also use that
+    my_print(&v[0..2]);
+    my_print(&a[2..]);
+    my_print(&slice_a[1..3]);
+}
+
+fn string_type() {
+    // the a is a pointer only.
+    // the string is stored in the pre-reserved readonly memory.
+    let a = "\"Ouch!\" said the well.\n";
+    print!("{}", a);
+    // also print string directly.
+    print!("my name is carlos\n");
+    print!("hello, hello, hello, this is the, and \
+            hello. \
+            \n");
+
+    // raw string
+    let b = r"C:\Program Files\App";
+    //let c = Regex::new(r"\d+(\.\d+)*");
+    println!(r###"
+            This raw string started with 'r###"'.
+            Therefore it does not end until we reach a quote mark ('"')
+            followed immediately by three pound signs ('###'):
+            "###);
+
+    // byte strings: every char is u8 rather than unicode text
+    let c = b"GET"; // &[u8; 3] array.
+    assert_eq!(c, &[b'G', b'E', b'T']);
+
+    let d = a.to_string(); // the a is in readonly reserved region,
+                           // the to_string copy it to heap.
+                           // the d is stack pointer that links the heap.
+    let d1 = &d[1..];
+    print!("{}", d1);
+
+    // The wrong case:
+    // no &mut for string
+    // let d2 = &mut d[5..];
+    // cannot be indexed.
+    // d2[3] = b'#';
+    // the index shall be [a..b], but its value cannot be changed.
+    let d2 = &d[8..9];
+    print!("d2 is {}\n", d2);
+
+    let mut e = String::new();
+    e = a.to_string();
+    println!("e is {}", e);
+
+    let f = String::from(a);
+    println!("f is {}", f);
+
+    // update string
+    // using push_str
+    e.push_str(" add frist ");
+    e.push_str(&f);
+    e.push_str(" add last ");
+    println!("e is {}", e);
+
+    // using +
+    let mut g = String::from("Hello ");
+    let g1 = "World!";
+    let g2 = g + &g1;
+    println!("g2 is {}. ", g2);
+
+    // using format
+    let s1 = String::from("C");
+    let s2 = String::from("is");
+    let s3 = String::from("a");
+    let s4 = String::from("programming");
+    let s5 = String::from("language.");
+    let s = format!("{} {} {} {} {}",s1,s2,s3,s4,s5);
+    println!("s is {}",s);
+
+    // iter chars
+    let s = "My name is Carlos\n";
+    for i in s.chars() {
+        print!("{}, ", i);
+    }
+
+    let k = vec!["veni", "vidi", "vici"];
+    assert_eq!(k.concat(), "venividivici");
+    assert_eq!(k.join(", "), "veni, vidi, vici");
+
+    let q = "ONE";
+    assert!(q.to_lowercase() == "one");
+    assert!("peanut".contains("nut"));
+    assert_eq!("hello_eye".replace("eye", "noise"), "hello_noise");
+    assert_eq!("  clean\n".trim(), "clean");
+
+    for word in "veni, vidi, vici".split(", ") {
+        assert!(word.starts_with("v"));
+    }
+}
+
 fn main() {
     reference_type();
     box_type();
     array_type();
     vector_type();
+    slice_type();
+    string_type();
     println!("Hello, world!");
 }
