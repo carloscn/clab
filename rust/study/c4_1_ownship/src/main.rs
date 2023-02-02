33b51ef214514a5eace50e39434d288ba522ed00
Carlos Wei <carlos.wei.hk@gmail.com>
2023-02-02 21:36:13 +0800

    [rust] add ownship
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

---
+++ rust/study/c4_1_ownship/src/main.rs
@@ -0,0 +1,15 @@
+
+fn test_move() {
+    // wrong case:
+    /*
+    let s = vec!["duon".to_string(), "ramen".to_string(), "soba".to_string()];
+    let t = s;
+    let u = t;
+    println!("{}", s[1]);   // the s and t memory move to the u;
+    */
+}
+
+fn main() {
+    test_move();
+    println!("Hello, world!");
+}
