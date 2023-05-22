563729c5a64248ecc1a490eaf811e76fd601d91d
carlos <carlos.wei.hk@gmail.com>
2023-05-22 08:38:27 +0800

    [rust-system] file operations
    Signed-off-by: carlos <carlos.wei.hk@gmail.com>
    Change-Id: I6924241811d7daa27ef7aab47a787d943712481c

--- rust/system/n01_file_operations/src/main.rs
+++ rust/system/n01_file_operations/src/main.rs
@@ -4,16 +4,18 @@ use std::io::{ErrorKind, Write, Read, self};
 
 mod utils_fo {
 
-    use std::fs::{OpenOptions, read};
+    use std::fs::{OpenOptions, read, remove_file};
     use std::f32::consts::E;
     use std::fs::File;
     use core::panic;
     use std::error::Error;
-    use std::io::{ErrorKind, Write, Read, self};
+    use std::io::{ErrorKind, Write, Read, self, BufRead};
 
-    pub fn write_file_ascii(file_name:&String) -> Result<(), io::Error>
+    pub fn write_file_ascii(file_name:&str, content:String) -> Result<(), io::Error>
     {
         let mut rc:Result<(), io::Error>;
+
+        drop_file(file_name);
         // open file
         let f = File::open(file_name);
         let mut f = match f {
@@ -28,21 +30,21 @@ mod utils_fo {
         };
 
         // Write ASCII to a File in Rust
-        rc = match f.write("helloworld".to_string().as_bytes()) {
+        rc = match f.write(content.as_bytes()) {
             Ok(_) => Ok(()),
             Err(error) => return Err(error),
         };
 
         // Optional, you can use the macro write!
-        rc = match write!(f, "dsflkshkglhsklg\n") {
-            Ok(_) => Ok(()),
-            Err(error) => return Err(error),
-        };
+        // rc = match write!(f, "dsflkshkglhsklg\n") {
+        //     Ok(_) => Ok(()),
+        //     Err(error) => return Err(error),
+        // };
 
         return rc;
     }
 
-    pub fn write_file_ascii_append(file_name:&String) -> Result<(), io::Error>
+    pub fn write_file_ascii_append(file_name:&str, content:String) -> Result<(), io::Error>
     {
         let mut rc:Result<(), io::Error>;
 
@@ -69,7 +71,7 @@ mod utils_fo {
             },
         };
 
-        rc = match f.write("append file hellop\n".as_bytes()) {
+        rc = match f.write(content.as_bytes()) {
             Ok(_) => Ok(()),
             Err(error) => {
                 println!("write failed!\n");
@@ -77,13 +79,13 @@ mod utils_fo {
             },
         };
 
-        rc = match write!(f, "append file hello using write!()\n") {
-            Ok(_) => Ok(()),
-            Err(error) => {
-                println!("write failed!\n");
-                return Err(error)
-            },
-        };
+        // rc = match write!(f, "append file hello using write!()\n") {
+        //     Ok(_) => Ok(()),
+        //     Err(error) => {
+        //         println!("write failed!\n");
+        //         return Err(error)
+        //     },
+        // };
 
         return rc;
     }
@@ -174,6 +176,103 @@ mod utils_fo {
 
         return rc;
     }
+
+    pub fn read_file_ascii(file_name:&str) -> Result<String, io::Error>
+    {
+        let rc:Result<String, io::Error>;
+
+        let mut ctx = match File::open(file_name) {
+            Ok(file) => file,
+            Err(err) => {
+                println!("file open error! {:?}", err);
+                return Err(err);
+            },
+        };
+
+        let mut read_str:String = String::new();
+        ctx.read_to_string(&mut read_str).unwrap();
+
+        rc = Ok(read_str);
+
+        return rc;
+    }
+
+    pub fn read_file_ascii_lines(file_name:&str) -> Result<io::Lines<io::BufReader<File>>, io::Error>
+    {
+        if file_name.is_empty() {
+            return Err(io::Error::new(ErrorKind::InvalidInput, "invalid filename"));
+        }
+
+        let ctx = match File::open(file_name) {
+            Ok(ctx) => ctx,
+            Err(err) => {
+                println!("open file failed. {:?}", err);
+                return Err(err);
+            },
+        };
+
+        Ok(io::BufReader::new(ctx).lines())
+    }
+
+    pub fn drop_file(file_name:&str) -> Result<(), io::Error>
+    {
+        use std::path::Path;
+        let target_path = Path::new(file_name);
+
+        return remove_file(target_path)
+    }
+
+    pub fn dup_file(file_name_in:&str, file_name_out:&str) -> Result<(), io::Error>
+    {
+        let mut rc:Result<(), io::Error>;
+
+        if file_name_in.is_empty() ||
+           file_name_out.is_empty() {
+            return Err(io::Error::new(
+                        ErrorKind::InvalidInput,
+                        "file name is empty!"));
+        }
+
+        let mut in_ctx = match File::open(file_name_in) {
+            Ok(file) => file,
+            Err(err) => {
+                println!("the input file open failed!");
+                return Err(err);
+            }
+        };
+
+        let mut out_ctx = match File::open(file_name_out) {
+            Ok(file) => file,
+            Err(err) => match err.kind() {
+                ErrorKind::NotFound => match File::create(file_name_out){
+                    Ok(file) => file,
+                    Err(err) => {
+                        println!("the file create failed {:?}", err);
+                        return Err(err);
+                    }
+                },
+                ErrorOthers => {
+                    println!("open file failed! {:?}", ErrorOthers);
+                    return Err(err);
+                }
+            },
+        };
+
+        let mut buffer = [0u8; 4096];
+
+        loop {
+            let nbytes = in_ctx.read(&mut buffer).unwrap();
+            out_ctx.write(&buffer[..nbytes]).unwrap();
+            if nbytes < buffer.len() {
+                break;
+            }
+        }
+
+        rc = Ok(());
+
+        return rc;
+    }
+
 }
 
 mod utils_dir {
@@ -207,14 +306,45 @@ fn main() {
 
     // let mut rc = utils_fo::write_file_ascii(&"hello.txt".to_string());
 
-    //let mut rc: Result<(), io::Error> = utils_fo::write_file_ascii_append(&"hello.txt".to_string());
+    // let mut rc: Result<(), io::Error> = utils_fo::write_file_ascii_append(&"hello.txt".to_string());
 
-    let input = vec![1u8, 2,8,9,4,1,2];
-    let rc = utils_fo::write_file_bin("hello.bin", &input);
+    // let input = vec![1u8, 2,8,9,4,1,2];
+    // let rc = utils_fo::write_file_bin("hello.bin", &input);
 
-    let rc = utils_fo::read_file_binary("hello.bin").unwrap_or_else(|err| {
-        panic!("read failed\n");
-    });
+    // let rc = utils_fo::read_file_binary("hello.bin").unwrap_or_else(|err| {
+    //     panic!("read failed\n");
+    // });
 
-    println!("read result is : {:?}", rc);
+    // println!("read result is : {:?}", rc);
+
+    // let rc = utils_fo::read_file_ascii("hello.txt").unwrap();
+    // println!("read result ascii is {:?}", rc);
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use crate::utils_fo::drop_file;
+
+    use super::*;
+
+    #[test]
+    fn fo_test_write_read_file_without_append() {
+        utils_fo::write_file_ascii("hello.txt", "hellow".to_string());
+        let ret = utils_fo::read_file_ascii("hello.txt").unwrap();
+        drop_file("hello.txt");
+        assert_eq!(ret, "hellow".to_string());
+
+    }
+
+    #[test]
+    fn fo_test_write_read_file_with_append() {
+        utils_fo::write_file_ascii("hello.txt", "hellow".to_string());
+        utils_fo::write_file_ascii_append("hello.txt", "world".to_string()).unwrap();
+        let ret = utils_fo::read_file_ascii("hello.txt").unwrap();
+        drop_file("hello.txt");
+        assert_eq!(ret, "hellowworld".to_string());
+    }
+
 }
